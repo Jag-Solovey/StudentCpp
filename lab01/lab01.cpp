a557f9f80f7432ad5227aca78d0c13d3dfcece60
Jag-Solovey <43543147+Jag-Solovey@users.noreply.github.com>
2018-09-27 08:34:24 +0500

    Add files via upload

--- lab01/lab01.cpp
+++ lab01/lab01.cpp
@@ -6,27 +6,37 @@ double log (double x, double d)
 {
     return log(d)/log(x);
 }
+//Защита от дурака. Что в нее входит: если происходит ошибка ввода,
+//то значения игнорируются и обнуляются до тех пор, пока не будет введено корректное значение
+float checking1 (float d)
+{
+    cin >> d;
+    while(cin.fail())
+    {
+        cout << "Nice joke, but I do not work like that. Try again!\n ";
+        cin.clear();
+        cin.ignore(6666,'\n');
+        cin >> d;
+    }
+    return d;
+}
+//Проверка того, что выбран существующий вариант
+int checking2 (int d, float x)
+{
+    if (d < 1 || d > 2) {
+        cout << "\nThere is no such option! :D";
+    } else {
+        cout << "\nResult: " << x;
+    }
+}
 int main() {
     int d, f = 1;
     float num1, num2, x;
     //Цикл для выполнения повторного использования калькулятора
     while (f == 1) {
         cout << "\nHello, user! What are the numbers?\n";
-
-        cin >> num1;
-        //Защита от дурака. Что в нее входит: если происходит ошибка ввода,
-        //то значения игнорируются и обнуляются до тех пор, пока не будет введено корректное значение
-        while (cin.fail()) {
-            cin.clear();
-            cin.ignore(6666, '\n');
-            cin >> num1;
-        }
-        cin >> num2;
-        while (cin.fail()) {
-            cin.clear();
-            cin.ignore(6666, '\n');
-            cin >> num2;
-        }
+        num1 = checking1(d);
+        num2 = checking1(d);
         cout << "\nNumber 1: " << num1 << "\nNumber 2: " << num2
              << "\nWhat do you want to do?\n1. +\n2. -\n3. *\n4. /\n5. ^\n6. sqrt\n7.log\n";
         //Если человек введет d<1 или d>7, то программа просто перезапустится
@@ -66,14 +76,8 @@ int main() {
             } else if (d == 2) {
                 x = num2 * num2;
             }
-            //Проверка того, что выбран существующий вариант
-            if (d < 1 || d > 2) {
-                cout << "\nThere is no such option! :D";
-            } else {
-                cout << "\nResult: " << x;
-            }
+            checking2(d, x);
         }
-
         //Поиск корня
         if (d == 6) {
             cout << "\nWith which of the numbers do the operation? \n1. " << num1 << "\n2. " << num2;
@@ -83,15 +87,11 @@ int main() {
             } else if (d == 2) {
                 x = sqrt(num2);
             }
-            if (d < 1 || d > 2) {
-                cout << "\nThere is no such option! :D";
-            } else {
-                cout << "\nResult: " << x;
-            }
+            checking2(d, x);
         }
-        if (d ==7) {
+        if (d == 7) {
             cout << "\nWith which of the numbers do the operation? \n1. " << num1 << "\n2. " << num2 <<
-            "\nAnd enter the base\n";
+                 "\nAnd enter the base\n";
             cin >> d;
             cin >> x;
             if (d == 1) {
@@ -105,12 +105,7 @@ int main() {
             }
         }
         cout << "\nDo you want to try again? \n1. yes\n2. no\n";
-        cin >> f;
-        while (cin.fail()) {
-            cin.clear();
-            cin.ignore(6666, '\n');
-            cin >> f;
-        }
+        f = checking1(f);
     }
     //Выполнил Соловьев Сергей КЭ-106
     return 0;
