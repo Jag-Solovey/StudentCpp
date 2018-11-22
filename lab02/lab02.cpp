88afba460548bd816168cdf0296176d67be94e4d
Jag-Solovey <43543147+Jag-Solovey@users.noreply.github.com>
2018-11-22 11:53:09 +0500

    Начал с двумерного массива

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {   //Инициализация массива
-    int **S1vy = new int*[2];
+    char **S1vy = new char*[2];
     for (int i = 0; i<2; i++)
-        S1vy[i] = new int [10];
-
-    cout << "Input constants:\nFor example: 357 4 5 end";
+        S1vy[i] = new char [10];
+    cout << "Input constants:\nFor example: 7 4 5 end";
     for (int i = 0; i<10; i++)
         cin >> S1vy[1][i];
+    //Проверка
     for (int i = 0; i<10; i++)
     cout << S1vy[1][i] << endl;
     return 0;
