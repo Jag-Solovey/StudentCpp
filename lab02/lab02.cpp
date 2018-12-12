28ec554327d8aabf3b7add87883178a3593b2cea
Jag-Solovey <43543147+Jag-Solovey@users.noreply.github.com>
2018-12-12 12:44:27 +0500

    Начало положено

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -2,14 +2,25 @@
 using namespace std;
 int main()
 {   //Инициализация массива
-    char **S1vy = new char*[2];
-    for (int i = 0; i<2; i++)
-        S1vy[i] = new char [10];
+    char *S1vy = new char[10];
+    int *Vy1S = new int[99];
     cout << "Input constants:\nFor example: 7 4 5 end";
-    for (int i = 0; i<10; i++)
-        cin >> S1vy[1][i];
+    for (int i = 0; i<99; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            S1vy[j] = {0};
+        }
+        cin >> S1vy;
+        if ((S1vy[0]=='e')&&(S1vy[1] == 'n')&&(S1vy[2] == 'd')&&(S1vy[3] == '\0'))
+        {break;};
+
+        Vy1S[i] = atoi(&S1vy[0]);
+    }
+    //for (int i = 0; i<10; i++)
+        //cin >> S1vy[2][i];
     //Проверка
     for (int i = 0; i<10; i++)
-    cout << S1vy[1][i] << endl;
+    cout << Vy1S[i] << endl;
     return 0;
 }
