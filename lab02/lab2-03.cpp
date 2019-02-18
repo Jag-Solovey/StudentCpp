c914e90f8b6bb1bc8dd3aba4b4a0a0cb8bafcf0a
Jag-Solovey <43543147+Jag-Solovey@users.noreply.github.com>
2019-02-18 20:10:20 +0500

    Финальная версия

--- lab02/lab2-03.cpp
+++ lab02/lab2-03.cpp
@@ -1,56 +1,77 @@
 #include <iostream>
 using namespace std;
 
-double res ()
-{
-    for (int i=0 ; ;)
-    {
-        char *S1vy = new char[66];
-        double *Vy1S = new double[2];
-        for (; ;)
-        {
-        cin >> S1vy;
-        if (*S1vy == '\n')
-            break;
-        }
-        while ((S1vy[i] != '+')||(S1vy[i] != '-')||(S1vy[i] != '*')||(S1vy[i] != '/'))
-            i++;
-        Vy1S[0] = atof(S1vy);
-        char *S1vy1 = new char[99];
-        int j = 0;
-        while (S1vy[i+1] != '\0')
-        {
-            S1vy1[j] = S1vy[i + 1];
-            j++;
-        }
-        Vy1S[1] = atof(S1vy1);
-        switch (S1vy[i])
-        {
-            case '+':
-                return Vy1S[0] + Vy1S[1];
-            case '-':
-                return Vy1S[0] - Vy1S[1];
-            case '*':
-                return Vy1S[0] * Vy1S[1];
-            case '/':
-                {
-                    if (Vy1S[1] == 0)
-                    {
-                        cout << "\nYou cant divide by zero.";
-                        exit(0);
-                    }
-                   else return Vy1S[0] / Vy1S[1];
-                }
-        }
-
-    }
-
-}
-
 int main()
 {
-    //Инициализация массива
-    cout << "Введите 2 числа и действие над ними.";
-    double R = res();
-    cout << "\nРезультат: " << R;
+    char *S1vy = new char[66]; //Массив для ввода
+    double *Vy1S = new double[2]; //Массив для чисел
+    cout << "Enter expression, please!\nFor example: 55+55";
+    cin.getline(S1vy,66); //Ввод массива
+    int i = 0;
+    for (i;i<66;i++) //Проверка на пробелы
+    {
+        if ((S1vy[i] == ' ')&&(S1vy[i + 1] != ' '))
+        {
+            cout << "Input error, try again :)";
+            exit(0);
+        }
+    }
+    cout << "\nInput the expression: " << S1vy;
+    i = 0;
+    while ((S1vy[i] != '+')&&(S1vy[i] != '-')&&(S1vy[i] != '*')&&(S1vy[i] != '/')) //Поиск индекса массива, хранящий действие над числами
+        i++;
+    int p = i; //Сохранение индекса
+    int j = i+2; //Скачек через действие
+    while (S1vy[j] != '\0') //Поиск окончания строки
+        j++;
+    char *S2vy = new char[66]; //Массив для выделения первого числа
+    int k = 0;
+    while (k != i)
+    {
+        S2vy[k]=S1vy[k];
+        k++;
+    }
+    Vy1S[0] = atof(S2vy); //Присвоение значения первого числа
+    delete [] S2vy;
+    char *S3vy = new char[66]; //Массив для выделения второго числа
+    k = k+1;
+    i = 0;
+    while (k <= j)
+    {
+        S3vy[i] = S1vy[k];
+        k++;
+        i++;
+    }
+    Vy1S[1] = atof(S3vy); //Присвоение значения второго числа
+    delete [] S3vy;
+    int R = 0;
+    switch (S1vy[p]) //Действие с числами
+    {
+        case '+':
+        {
+            R = Vy1S[0] + Vy1S[1];
+            break;
+        }
+        case '-':
+        {
+            R = Vy1S[0] - Vy1S[1];
+            break;
+        }
+        case '*':
+        {
+            R = Vy1S[0] * Vy1S[1];
+            break;
+        }
+        case '/':
+        {
+            if  (Vy1S[1] == 0)
+            {
+                cout << "\nYou cant divide by zero.";
+                exit(0);
+            }
+            R = Vy1S[0]/Vy1S[1];
+            break;
+        }
+    }
+    cout << "\nResult:" << R;
 }
