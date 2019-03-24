3fe937aad453c7d8db02ae964006be2332b6596a
Jag-Solovey <43543147+Jag-Solovey@users.noreply.github.com>
2019-03-24 22:05:34 +0500

    Готовая 2-я часть(с функциями)

--- lab02/lab2-02.cpp
+++ lab02/lab2-02.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int main()
+int mAr(char* S1vy, double* Vy1S)
 {
-    //Инициализация массива
-    char *S1vy = new char[10];
-    double *Vy1S = new double[99];
-    //Ввод переменных
-    cout << "Input constants:\nFor example: 1.75 0.21 9 end";
     for (int i = 0; i<99; i++)
     {
         for (int j = 0; j < 16; j++)
@@ -29,15 +23,15 @@ int main()
         Vy1S[i] = atof(S1vy);
     }
     int n=0;
-    //Нахождение количества эл-тов массива
-    while ((Vy1S[n] != 0)||(Vy1S[n+1] != 0))
+    while ((Vy1S[n] != 0)||(Vy1S[n+1] != 0)) //Нахождение количества эл-тов массива
     {
         n++;
     }
-    //Присвоение значения 1-го эл-та массива для написания формулы поиска результата
-    double R = Vy1S[0];
-    //Ввод действий и нахождение результата
-    cout << "Input actions:\nFor example: + - * /";
+    return n;
+}
+void mAr(double* Vy1S, int n, char* S1vy)
+{
+    double R = Vy1S[0]; //Присвоение значения 1-го эл-та массива для написания формулы поиска результата
     for (int i = 0; i<(n-1); i++)
     {
         for (int j = 0; j < n; j++)
@@ -78,4 +72,15 @@ int main()
     }
     //Вывод результата
     cout << "\nResult: " << R;
+}
+int main()
+{
+    //Инициализация массива
+    char *S1vy = new char[10];
+    double *Vy1S = new double[99];
+    //Ввод переменных
+    cout << "Input constants:\nFor example: 1.75 0.21 9 end";
+    int n = mAr(S1vy, Vy1S);   //Ввод переменных и подсчет количества элементов
+    cout << "Input actions:\nFor example: + - * /";   //Ввод действий и нахождение результата
+    mAr(Vy1S, n, S1vy);    //Функция подсчета результата и его вывода на экран
 }
