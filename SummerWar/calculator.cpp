4c2a437199c26265a3f270b3586015279fcf1c3d
Jag-Solovey <43543147+Jag-Solovey@users.noreply.github.com>
2019-07-10 16:53:53 +0500

    46%

--- SummerWar/calculator.cpp
+++ SummerWar/calculator.cpp
@@ -3,6 +3,10 @@
 bool dot_click = false;
 bool sqrt_click = false;
 bool pow_click = false;
+bool plus_click = false;
+bool minus_click = false;
+bool division_click = false;
+bool mult_click = false;
 Calculator::Calculator(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Calculator)
@@ -18,6 +22,11 @@ Calculator::Calculator(QWidget *parent) :
     connect(ui->pushButton_7, SIGNAL(clicked()), this, SLOT(digits_numbers()));
     connect(ui->pushButton_8, SIGNAL(clicked()), this, SLOT(digits_numbers()));
     connect(ui->pushButton_9, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_plus, SIGNAL(clicked()), this, SLOT(actions_with_numbers()));
+    connect(ui->pushButton_minus, SIGNAL(clicked()), this, SLOT(actions_with_numbers()));
+    connect(ui->pushButton_mult, SIGNAL(clicked()), this, SLOT(actions_with_numbers()));
+    connect(ui->pushButton_division, SIGNAL(clicked()), this, SLOT(actions_with_numbers()));
+}
 }
 
 Calculator::~Calculator()
@@ -28,10 +37,12 @@ void Calculator::digits_numbers()
 {
     QPushButton *button = (QPushButton*)sender();
     double all_numbers;
-    QString Label;
-    all_numbers = (ui->result_show->text()+button->text()).toDouble();
-    Label = QString::number(all_numbers);
-    ui->result_show->setText(Label, 'g', 15);
+    all_numbers = (ui->result_show->text()+button->text()).toQString();
+    ui->result_show->setText(all_numbers, 'g', 50);
+    plus_click = false;
+    minus_click = false;
+    division_click = false;
+    mult_click = false;
 }
 void Calculator::on_pushButton_point_clicked()
 {
@@ -58,5 +69,49 @@ void Calculator::on_pushButton_sqrt_clicked()
         ui->result_show->setText(ui->result_show->text()+ "√");
         sqrt_click = true;
         dot_click = false;
+    }
+}
+void Calculator::actions_with_numbers()
+{
+    QPushButton *button = (QPushButton*)sender();
+    if(!plus_click)
+    {
+        ui->result_show->setText(ui->result_show->text()+ "+");
+        plus_click = true;
+        minus_click = true;
+        mult_click = true;
+        division_click = true;
+        dot_click = false;
+        sqrt_click = false;
+    }
+    else if (!minus_click)
+    {
+        ui->result_show->setText(ui->result_show->text()+ "-");
+        plus_click = true;
+        minus_click = true;
+        mult_click = true;
+        division_click = true;
+        dot_click = false;
+        sqrt_click = false;
+    }
+    else if (!mult_click)
+    {
+        ui->result_show->setText(ui->result_show->text()+ "*");
+        plus_click = true;
+        minus_click = true;
+        mult_click = true;
+        division_click = true;
+        dot_click = false;
+        sqrt_click = false;
+    }
+    else if (!division_click)
+    {
+        ui->result_show->setText(ui->result_show->text()+ "/");
+        plus_click = true;
+        minus_click = true;
+        mult_click = true;
+        division_click = true;
+        dot_click = false;
+        sqrt_click = false;
     }
 }
